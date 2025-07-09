4008a82207325e0ccac2e152cc49354a753cdc52
Muhammad Jazib <mjxdex@gmail.com>
2025-07-09 23:31:30 +0500

    init

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+"use client"
+import React, { useState } from 'react'
+import toast, { SetupNiceToaster } from 'nice-toaster'
+
+const page = () => {
+  const [theme, setTheme] = useState("light");
+  const notify = () => { toast.success("Cool", { vibration: true, notification: true, timer: true }) }
+  return (
+    <>
+      <SetupNiceToaster settings={{ theme }} />
+      <button onClick={notify}>Click me</button>
+      <button onClick={() => { setTheme("light") }}>light</button>
+      <button onClick={() => { setTheme("dark") }}>dark</button>
+      okkkkkk
+    </>
+  )
+}
+
+export default page
